3e047240a128e672548f1501017b9850608d74ef
Martin Alfke <ma@example42.com>
2015-08-03 16:10:17 +0200

    rspec tests
    - run bundle exec rake instead of rake on travis
    - pin rspec to 3.1.x on ruby 1.8.x
    - add puppetlabs/stdlib as spec dependency
    - remove rspec deprecations

--- spec/classes/puppetdashboard_spec.rb
+++ spec/classes/puppetdashboard_spec.rb
@@ -26,12 +26,10 @@ describe 'puppetdashboard' do
     it { should contain_service('puppetdashboard').with_enable('true') }
     it { should contain_file('puppetdashboard.conf').with_ensure('present') }
     it 'should monitor the process' do
-      content = catalogue.resource('monitor::process', 'puppetdashboard_process').send(:parameters)[:enable]
-      content.should == true
+      should contain_monitor__process('puppetdashboard_process').with_enable(true)
     end
     it 'should place a firewall rule' do
-      content = catalogue.resource('firewall', 'puppetdashboard_tcp_42').send(:parameters)[:enable]
-      content.should == true
+      should contain_firewall('puppetdashboard_tcp_42').with_enable(true)
     end
   end
 
@@ -43,12 +41,10 @@ describe 'puppetdashboard' do
     it 'should not enable at boot Service[puppetdashboard]' do should contain_service('puppetdashboard').with_enable('false') end
     it 'should remove puppetdashboard configuration file' do should contain_file('puppetdashboard.conf').with_ensure('absent') end
     it 'should not monitor the process' do
-      content = catalogue.resource('monitor::process', 'puppetdashboard_process').send(:parameters)[:enable]
-      content.should == false
+      should contain_monitor__process('puppetdashboard_process').with_enable(false)
     end
     it 'should remove a firewall rule' do
-      content = catalogue.resource('firewall', 'puppetdashboard_tcp_42').send(:parameters)[:enable]
-      content.should == false
+      should contain_firewall('puppetdashboard_tcp_42').with_enable(false)
     end
   end
 
@@ -60,12 +56,10 @@ describe 'puppetdashboard' do
     it 'should not enable at boot Service[puppetdashboard]' do should contain_service('puppetdashboard').with_enable('false') end
     it { should contain_file('puppetdashboard.conf').with_ensure('present') }
     it 'should not monitor the process' do
-      content = catalogue.resource('monitor::process', 'puppetdashboard_process').send(:parameters)[:enable]
-      content.should == false
+      should contain_monitor__process('puppetdashboard_process').with_enable(false)
     end
     it 'should remove a firewall rule' do
-      content = catalogue.resource('firewall', 'puppetdashboard_tcp_42').send(:parameters)[:enable]
-      content.should == false
+      should contain_firewall('puppetdashboard_tcp_42').with_enable(false)
     end
   end
 
@@ -78,12 +72,10 @@ describe 'puppetdashboard' do
     it 'should not enable at boot Service[puppetdashboard]' do should contain_service('puppetdashboard').with_enable('false') end
     it { should contain_file('puppetdashboard.conf').with_ensure('present') }
     it 'should not monitor the process locally' do
-      content = catalogue.resource('monitor::process', 'puppetdashboard_process').send(:parameters)[:enable]
-      content.should == false
+      should contain_monitor__process('puppetdashboard_process').with_enable(false)
     end
     it 'should keep a firewall rule' do
-      content = catalogue.resource('firewall', 'puppetdashboard_tcp_42').send(:parameters)[:enable]
-      content.should == true
+      should contain_firewall('puppetdashboard_tcp_42').with_enable(true)
     end
   end 
 
@@ -91,12 +83,10 @@ describe 'puppetdashboard' do
     let(:params) { {:template => "puppetdashboard/spec.erb" , :options => { 'opt_a' => 'value_a' } } }
 
     it 'should generate a valid template' do
-      content = catalogue.resource('file', 'puppetdashboard.conf').send(:parameters)[:content]
-      content.should match "fqdn: rspec.example42.com"
+      should contain_file('puppetdashboard.conf').with_content(/fqdn: rspec.example42.com/)
     end
     it 'should generate a template that uses custom options' do
-      content = catalogue.resource('file', 'puppetdashboard.conf').send(:parameters)[:content]
-      content.should match "value_a"
+      should contain_file('puppetdashboard.conf').with_content(/value_a/)
     end
 
   end
@@ -105,24 +95,20 @@ describe 'puppetdashboard' do
     let(:params) { {:source => "puppet://modules/puppetdashboard/spec" , :source_dir => "puppet://modules/puppetdashboard/dir/spec" , :source_dir_purge => true } }
 
     it 'should request a valid source ' do
-      content = catalogue.resource('file', 'puppetdashboard.conf').send(:parameters)[:source]
-      content.should == "puppet://modules/puppetdashboard/spec"
+      should contain_file('puppetdashboard.conf').with_source("puppet://modules/puppetdashboard/spec")
     end
     it 'should request a valid source dir' do
-      content = catalogue.resource('file', 'puppetdashboard.dir').send(:parameters)[:source]
-      content.should == "puppet://modules/puppetdashboard/dir/spec"
+      should contain_file('puppetdashboard.dir').with_source("puppet://modules/puppetdashboard/dir/spec")
     end
     it 'should purge source dir if source_dir_purge is true' do
-      content = catalogue.resource('file', 'puppetdashboard.dir').send(:parameters)[:purge]
-      content.should == true
+      should contain_file('puppetdashboard.dir').with_purge(true)
     end
   end
 
   describe 'Test customizations - custom class' do
     let(:params) { {:my_class => "puppetdashboard::spec" } }
     it 'should automatically include a custom class' do
-      content = catalogue.resource('file', 'puppetdashboard.conf').send(:parameters)[:content]
-      content.should match "fqdn: rspec.example42.com"
+      should contain_file('puppetdashboard.conf').with_content(/fqdn: rspec.example42.com/)
     end
   end
 
@@ -134,8 +120,7 @@ describe 'puppetdashboard' do
     let(:params) { {:service_autorestart => "no" } }
 
     it 'should not automatically restart the service, when service_autorestart => false' do
-      content = catalogue.resource('file', 'puppetdashboard.conf').send(:parameters)[:notify]
-      content.should be_nil
+      should contain_file('puppetdashboard.conf').with_notify(nil)
     end
   end
 
@@ -143,8 +128,7 @@ describe 'puppetdashboard' do
     let(:params) { {:puppi => true, :puppi_helper => "myhelper"} }
 
     it 'should generate a puppi::ze define' do
-      content = catalogue.resource('puppi::ze', 'puppetdashboard').send(:parameters)[:helper]
-      content.should == "myhelper"
+      should contain_puppi__ze('puppetdashboard').with_helper("myhelper")
     end
   end
 
@@ -152,8 +136,7 @@ describe 'puppetdashboard' do
     let(:params) { {:monitor => true, :monitor_tool => "puppi" } }
 
     it 'should generate monitor defines' do
-      content = catalogue.resource('monitor::process', 'puppetdashboard_process').send(:parameters)[:tool]
-      content.should == "puppi"
+      should contain_monitor__process('puppetdashboard_process').with_tool("puppi")
     end
   end
 
@@ -161,8 +144,7 @@ describe 'puppetdashboard' do
     let(:params) { {:firewall => true, :firewall_tool => "iptables" , :protocol => "tcp" , :port => "42" } }
 
     it 'should generate correct firewall define' do
-      content = catalogue.resource('firewall', 'puppetdashboard_tcp_42').send(:parameters)[:tool]
-      content.should == "iptables"
+      should contain_firewall('puppetdashboard_tcp_42').with_tool("iptables")
     end
   end
 
@@ -170,16 +152,13 @@ describe 'puppetdashboard' do
     let(:params) { {:monitor => "yes" , :monitor_tool => "puppi" , :firewall => "yes" , :firewall_tool => "iptables" , :puppi => "yes" , :port => "42" , :protocol => 'tcp' } }
 
     it 'should generate monitor resources' do
-      content = catalogue.resource('monitor::process', 'puppetdashboard_process').send(:parameters)[:tool]
-      content.should == "puppi"
+      should contain_monitor__process('puppetdashboard_process').with_tool("puppi")
     end
     it 'should generate firewall resources' do
-      content = catalogue.resource('firewall', 'puppetdashboard_tcp_42').send(:parameters)[:tool]
-      content.should == "iptables"
+      should contain_firewall('puppetdashboard_tcp_42').with_tool("iptables")
     end
     it 'should generate puppi resources ' do 
-      content = catalogue.resource('puppi::ze', 'puppetdashboard').send(:parameters)[:ensure]
-      content.should == "present"
+      should contain_puppi__ze('puppetdashboard').with_ensure("present")
     end
   end
 
@@ -188,8 +167,7 @@ describe 'puppetdashboard' do
     let(:params) { { :port => '42' } }
 
     it 'should honour top scope global vars' do
-      content = catalogue.resource('monitor::process', 'puppetdashboard_process').send(:parameters)[:enable]
-      content.should == true
+      should contain_monitor__process('puppetdashboard_process').with_enable(true)
     end
   end
 
@@ -198,8 +176,7 @@ describe 'puppetdashboard' do
     let(:params) { { :port => '42' } }
 
     it 'should honour module specific vars' do
-      content = catalogue.resource('monitor::process', 'puppetdashboard_process').send(:parameters)[:enable]
-      content.should == true
+      should contain_monitor__process('puppetdashboard_process').with_enable(true)
     end
   end
 
@@ -208,8 +185,7 @@ describe 'puppetdashboard' do
     let(:params) { { :port => '42' } }
 
     it 'should honour top scope module specific over global vars' do
-      content = catalogue.resource('monitor::process', 'puppetdashboard_process').send(:parameters)[:enable]
-      content.should == true
+      should contain_monitor__process('puppetdashboard_process').with_enable(true)
     end
   end
 
@@ -218,8 +194,7 @@ describe 'puppetdashboard' do
     let(:params) { { :monitor => true , :firewall => true, :port => '42' } }
 
     it 'should honour passed params over global vars' do
-      content = catalogue.resource('monitor::process', 'puppetdashboard_process').send(:parameters)[:enable]
-      content.should == true
+      should contain_monitor__process('puppetdashboard_process').with_enable(true)
     end
   end
 
